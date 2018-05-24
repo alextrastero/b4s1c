2fab176c21face2d0363b3cc36d980ff77cce2de
Alexander Odell <alextrastero@gmail.com>
2018-05-24 12:28:37 +0200

    Update

--- gulpfile.js
+++ gulpfile.js
@@ -1,30 +1,29 @@
-var gulp = require('gulp'),
-    sourcemaps = require('gulp-sourcemaps'),
-    plumber = require('gulp-plumber'),
-    watch = require('gulp-watch'),
-    livereload = require('gulp-livereload'),
-    sass = require('gulp-sass');
+var gulp = require('gulp')
+var sourcemaps = require('gulp-sourcemaps')
+var plumber = require('gulp-plumber')
+var livereload = require('gulp-livereload')
+var sass = require('gulp-sass')
 
-gulp.task('sass', function() {
+gulp.task('sass', function () {
   gulp.src('*.scss')
     .pipe(sourcemaps.init())
     .pipe(plumber())
     .pipe(sass())
     .pipe(sourcemaps.write())
     .pipe(gulp.dest('.'))
-    .pipe(livereload());
-});
+    .pipe(livereload())
+})
 
-gulp.task('watch', function() {
-  livereload.listen();
-  gulp.watch('*.scss', ['sass']);
-});
+gulp.task('watch', function () {
+  livereload.listen()
+  gulp.watch('*.scss', ['sass'])
+})
 
-gulp.task('express', function() {
-  var express = require('express');
-  var app = express();
-  app.use(express.static(__dirname));
-  app.listen(3000);
-});
+gulp.task('express', function () {
+  var express = require('express')
+  var app = express()
+  app.use(express.static(__dirname))
+  app.listen(3000)
+})
 
-gulp.task('default', ['watch', 'sass', 'express']);
+gulp.task('default', ['watch', 'sass', 'express'])
